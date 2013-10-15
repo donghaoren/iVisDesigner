a78c038b5858ea95359cfc83f9ea82cfed68a573
Donghao Ren <donghao.ren@gmail.com>
2013-10-15 09:16:27 -0700

    Added today into the filename.

--- archive.py
+++ archive.py
@@ -2,6 +2,11 @@
 
 execfile("version.py")
 
-cmd = "git archive --format=tar.gz --prefix=iVisDesigner-%s/ -o dist/iVisDesigner-%s-r%s.tar.gz HEAD" % (IV_version, IV_version, IV_rev)
+import datetime
+
+today = datetime.date.today().strftime("%Y%m%d")
+
+cmd = "git archive --format=tar.gz --prefix=iVisDesigner-%s/ -o dist/iVisDesigner-%s-%s-r%s.tar.gz HEAD" % \
+    (IV_version, IV_version, today, IV_rev)
 print cmd
 commands.getoutput(cmd)
